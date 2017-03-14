d9d54b86a55a6dfb3d09cfb459f4b79156e618f9
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2017-03-14 15:21:36 +0100

    Vintage romantic

---
+++ src/20170308/bb.js
@@ -0,0 +1,5 @@
+var head = document.getElementsByTagName("head")[0],
+    link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://folk.ntnu.no/sverrsti/bb-css/20170308/bb.css";
+head.appendChild(link);
--- src/20170314/pure.js
+++ src/20170314/pure.js
@@ -1,5 +1,5 @@
 var head = document.getElementsByTagName("head")[0],
     link = document.createElement("link");
 link.rel = "stylesheet";
-link.href = "https://folk.ntnu.no/sverrsti/bb/bb.css";
+link.href = "https://folk.ntnu.no/sverrsti/bb-css/20170314/pure.css";
 head.appendChild(link);
